614bab25b312b90d731475e1922522f29e7c708b
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-03-06 14:30:25 +0330

    add section

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,5 +1,18 @@
+import { Box } from '@mui/material';
+import newWoman from '../assets/new-woman.png';
 const Section2 = () => {
-  return <div>Section2</div>;
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '120vh',
+        backgroundColor: '#F4B82A',
+        paddingY: 4,
+      }}
+    >
+      <img src={newWoman} alt="" style={{ width: '100%', height: '100%' }} />
+    </Box>
+  );
 };
 
 export default Section2;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomePageBanner from '../components/HomePageBanner';
+import Section2 from '../components/Section2';
 
 const HomePage = () => {
   return (
     <>
       <HomePageBanner />
+      <Section2 />
     </>
   );
 };
